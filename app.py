6b2a0222e91d9bae636f29b2d1f99128406b8253
khikisb <89850965+khikisb@users.noreply.github.com>
2023-12-14 01:40:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,51 @@
 import streamlit as st
-import pandas as pd
 import joblib
+import re
 
-data = pd.read_csv("https://gist.githubusercontent.com/khikisb/db966a30f5341a31d8429885ad522e82/raw/90e5bdecaa24a3bf1a0a4f45b70d51274e7a337f/data_label_news.csv")
-data = data.reset_index(drop=True)
-df = data[['Content', 'Label']]
-
-# Memuat kembali fungsi-fungsi preprocessing
-clean_punct = joblib.load('model/clean_punct_function.joblib')
+# Load model dan fungsi-fungsi preprocessing
 tokenize_text = joblib.load('model/tokenize_text_function.joblib')
 remove_stopwords = joblib.load('model/remove_stopwords_function.joblib')
+tfidf_model = joblib.load('model/tfidf_model.joblib')  # Sesuaikan dengan model yang digunakan
+lda_model = joblib.load('model/lda_model.joblib')  # Sesuaikan dengan model yang digunakan
+knn_model = joblib.load('model/knn_model.joblib')  # Sesuaikan dengan model yang digunakan
 
-# Memuat kembali model TF-IDF, LDA, dan KNN
-tfidf_model = joblib.load('model/tfidf_model.joblib')
-lda_model = joblib.load('model/lda_model.joblib')
-knn_model = joblib.load('model/knn_model.joblib')
+# Fungsi untuk membersihkan tanda baca
+def clean_punct(text):
+    clean_tag = re.compile('@\S+')
+    clean_url = re.compile('https?:\/\/.*[\r\n]*')
+    clean_hastag = re.compile('#\S+')
+    clean_symbol = re.compile('[^a-zA-Z]')
+    text = clean_tag.sub('', str(text))
+    text = clean_url.sub('', text)
+    text = clean_hastag.sub(' ', text)
+    text = clean_symbol.sub(' ', text)
+    return text
 
-# Mendefinisikan aplikasi Streamlit
-st.title('Aplikasi Prediksi Klasifikasi Berita')
-user_input = st.text_area('Masukkan teks')
+st.title('Prediksi Label Teks')
 
-# Fungsi untuk melakukan prediksi pada teks baru
-def predict_new_data(text):
-    cleaned_data = clean_punct(text)
-    tokenized_data = tokenize_text(cleaned_data)
-    stopword_removed = remove_stopwords(tokenized_data)
-    tfidf_new_data = tfidf_model.transform(stopword_removed)
-    lda_new_data = lda_model.transform(tfidf_new_data)
-    predicted = knn_model.predict(lda_new_data)
-    return predicted
+# Input teks baru dari pengguna
+new_data = st.text_area('Masukkan teks baru:', '')
 
-# Tombol prediksi
 if st.button('Prediksi'):
-    if user_input:
-        prediction = predict_new_data([user_input])
-        st.write('Hasil Prediksi:', prediction)
+    if new_data:
+        # Pra-Pemrosesan teks baru
+        cleaned_data = clean_punct(new_data)
+        tokenized_data = tokenize_text(cleaned_data)
+        stopword_removed = remove_stopwords(tokenized_data)
+
+        # Mengonversi setiap elemen dalam stopword_removed menjadi string
+        stopword_removed_str = [' '.join(tokens) for tokens in stopword_removed]
+
+        # Transformasi TF-IDF pada data baru
+        tfidf_new_data = tfidf_model.transform(stopword_removed_str)
+
+        # Transformasi LDA pada data baru
+        lda_new_data = lda_model.transform(tfidf_new_data)
+
+        # Prediksi dengan model KNN pada data baru yang telah diproses
+        predicted = knn_model.predict(lda_new_data)
+
+        # Tampilkan hasil prediksi
+        st.write('Hasil Prediksi:', predicted)
     else:
-        st.write('Masukkan teks Berita untuk melakukan prediksi.')
+        st.warning('Silakan masukkan teks untuk melakukan prediksi.')
