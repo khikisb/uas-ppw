b147f0b38a7ed7ae693f20255580e24ccfae7bee
khikisb <89850965+khikisb@users.noreply.github.com>
2023-12-14 00:38:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import joblib
 
-# Muat kembali fungsi-fungsi preprocessing dan model yang sudah disimpan sebelumnya
+# Memuat kembali fungsi-fungsi preprocessing
 clean_punct = joblib.load('clean_punct_function.joblib')
 tokenize_text = joblib.load('tokenize_text_function.joblib')
 remove_stopwords = joblib.load('remove_stopwords_function.joblib')
+
+# Memuat kembali model TF-IDF, LDA, dan KNN
 tfidf_model = joblib.load('tfidf_model.joblib')
 lda_model = joblib.load('lda_model.joblib')
 knn_model = joblib.load('knn_model.joblib')
+
+# Mendefinisikan aplikasi Streamlit
+st.title('Aplikasi Prediksi')
+user_input = st.text_area('Masukkan teks')
 
 # Fungsi untuk melakukan prediksi pada teks baru
 def predict_new_data(text):
@@ -19,17 +25,10 @@ def predict_new_data(text):
     predicted = knn_model.predict(lda_new_data)
     return predicted
 
-# Judul aplikasi dan deskripsi
-st.title('Aplikasi Prediksi')
-st.write('Masukkan teks untuk diprediksi kategori nya:')
-
-# Input teks dari pengguna
-user_input = st.text_area('Teks')
-
 # Tombol prediksi
 if st.button('Prediksi'):
     if user_input:
         prediction = predict_new_data([user_input])
         st.write('Hasil Prediksi:', prediction)
     else:
-        st.write('Masukkan teks untuk melakukan prediksi.')
+        st.write('Masukkan teks Berita untuk melakukan prediksi.')
